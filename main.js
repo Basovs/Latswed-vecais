6ecf5e1948ce2760dedbe5f9ef1f1986bc293fb8
Janis Basovs <janisbasovs@Janiss-MacBook.lan>
2019-12-26 02:29:46 +0100

    final cleanup?

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-window.onscroll = function() {changeOnScroll()}
+window.onscroll = function() {
+    changeOnScroll()
+}
 
 function changeOnScroll() {
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-    document.querySelector('nav').className = 'onScroll'
-    // const allA = document.querySelectorAll('nav ul li a')
-    // for (let i = 0; i < allA.length; i++) {
-    //   allA[i].className = 'changeTextColor'
-    // }
-  } else {
-    document.querySelector('nav').className = ''
-    // const allA = document.querySelectorAll('nav ul li a')
-    // for (let i = 0; i < allA.length; i++) {
-    //   allA[i].className = ''
-    // }
-  }
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+        document.querySelector("nav").className = "onScroll"
+    } else {
+        document.querySelector("nav").className = ""
+    }
 }
